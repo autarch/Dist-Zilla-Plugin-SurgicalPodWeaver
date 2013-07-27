d7113e37ddb97431a4c398b0cfdc5725d9151bed
Robert Krimen <robertkrimen@gmail.com>
2013-07-26 19:38:52 -0700

    Initial commit

---
+++ lib/Dist/Zilla/Plugin/SurgicalPodWeaver.pm
@@ -0,0 +1,57 @@
+package Dist::Zilla::Plugin::SurgicalPodWeaver;
+# ABSTRACT: Surgically apply PodWeaver
+
+=head1 SYNOPSIS
+
+In your L<Dist::Zilla> C<dist.ini>:
+
+    [SurgicalPodWeaver]
+
+To hint that you want to apply PodWeaver:
+
+    package Xyzzy;
+    # Dist::Zilla: +PodWeaver
+
+    ...
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::SurgicalPodWeaver will only PodWeaver a .pm if:
+
+    1. There exists an # ABSTRACT: ...
+    2. The +PodWeaver hint is present
+
+If either condition is satisfied, PodWeavering will be done.
+
+You can forcefully disable PodWeaver on a .pm by using the C<-PodWeaver> hint
+
+=cut
+
+use Moose;
+extends qw/ Dist::Zilla::Plugin::PodWeaver /;
+
+require Dist::Zilla::PluginBundle::ROKR;
+
+around munge_pod => sub {
+    my $inner = shift;
+    my ( $self, $file ) = @_;
+
+    my $content = $file->content;
+
+    my $yes = 0;
+    if ( my $hint = Dist::Zilla::PluginBundle::ROKR->parse_hint( $content ) ) {
+        if ( exists $hint->{PodWeaver} ) {
+            return unless $hint->{PodWeaver};
+            $yes = 1;
+        }
+    }
+
+    if ( $yes || $content =~ m/^\s*#+\s*(?:ABSTRACT):\s*(.+)$/m ) { }
+    else { return }
+
+    return $inner->( @_ )
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
